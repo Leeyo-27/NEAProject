dadf14cf99bdc553502809a6438f343a7c9790ad
Leeyo-27 <127449077+Leeyo-27@users.noreply.github.com>
2023-03-10 19:49:57 +0000

    Add files via upload

---
+++ Settings.py
@@ -0,0 +1,31 @@
+import random
+
+# Constants
+WIDTH = 600
+HEIGHT = 800
+FPS = 60
+WHITE = 255, 255, 255
+DBLUE = 55, 80, 170
+LBLUE = 75, 180, 200
+RED = 190, 80, 60
+GREEN = 8, 108, 52
+FONT = "Aharoni"
+
+# Player properties
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.115
+
+# Questions
+
+SIGN = ["+", "-", "*", "/"]
+ADDNUM1 = range(1, 100)
+ADDNUM2 = range(1, 100)
+
+SUBNUM1 = range(1, 100)
+SUBNUM2 = range(1, 100)
+
+MULTNUM1 = range(1, 10)
+MULTNUM2 = range(1, 10)
+
+DIVNUM1 = range(50)
+DIVNUM2 = [1, 2, 4, 5, 10]
